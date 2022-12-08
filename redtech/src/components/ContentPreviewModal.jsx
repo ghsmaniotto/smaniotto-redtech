2ad84e40cac83847d80ec0206c630e26d44d3b78
Gustavo Smaniotto <ghsmaniotto@gmail.com>
2022-12-08 23:18:01 +0100

    Add ContentPreviewModal component
    - Encapsulate the ContentByTopic in the ContentPreviewModel in order
    to show the preview of each item

---
+++ redtech/src/components/ContentPreviewModal.jsx
@@ -0,0 +1,69 @@
+import { Dialog, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import ContentListItem from './ContentListItem'
+
+export default function ContentPreviewModal({content}) {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+  return (
+    <>
+      <div onClick={openModal}>
+        <ContentListItem content={content}/>
+      </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-center align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-large leading-7 text-gray-800"
+                  >
+                    Content Preview
+                  </Dialog.Title>
+                  {
+                    content.isImage() ?
+                      <img className="h-full w-full" src={content.previewURL} alt={content.fileName} /> :
+                      <video className="h-full w-full" controls>
+                        <source src={content.previewURL} />
+                      </video>
+                  }
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
+
--- redtech/src/pages/ContentByTopic.jsx
+++ redtech/src/pages/ContentByTopic.jsx
@@ -1,27 +1,29 @@
 import { useParams } from 'react-router-dom';
 import { Pixabay } from "../services/pixabay";
 import HeaderWithCompanyLogo from "../components/HeaderWithCompanyLogo";
-import ContentListItem from "../components/ContentListItem";
+import ContentPreviewModal from '../components/ContentPreviewModal';
 
 export default function ContentByTopic() {
     const { topic } = useParams();
 
     const pixabayService = new Pixabay();
     
-    let contentItems = pixabayService.getImages(topic);
+    let contentItems = pixabayService.getVideos(topic);
 
     return (
         <div className="min-h-full">
             <HeaderWithCompanyLogo title={topic} />
-            <div className="flow-root">
-                <ul className="divide-y divide-gray-200">
-                    {
-                        contentItems.map((content, index) => (
-                            <ContentListItem key={`content-item-${index}`} content={content} />
-                        ))
-                    }
-                </ul>
-            </div>
+            <main>
+                <div className="mx-auto max-w-2xl px-4 sm:py-6 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <ul className="divide-y divide-gray-200">
+                        {
+                            contentItems.map((contentItem, index) => (
+                                <ContentPreviewModal key={`content-item-${index}`} content={contentItem} />
+                            ))
+                        }
+                    </ul>
+                </div>
+            </main>
         </div>
       )
 }
