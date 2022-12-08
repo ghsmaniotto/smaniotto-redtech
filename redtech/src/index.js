1f9014198c234b704409775fb7e836a6d1907700
Gustavo Smaniotto <ghsmaniotto@gmail.com>
2022-12-08 13:31:02 +0100

    Add login and content page templates

--- redtech/src/index.js
+++ redtech/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css';
+import Login from "./pages/Login";
+import Content from "./pages/Content";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/content" element={<Content />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ redtech/src/pages/Content.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Content extends React.Component {
+    render() {
+        return (<>Content Page</>)
+    }
+}
