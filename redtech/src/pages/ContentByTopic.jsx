527165c38867e51f430fd6659a415bc1f915d5c9
Gustavo Smaniotto <ghsmaniotto@gmail.com>
2022-12-10 08:22:39 +0100

    Import useParams from react-router

--- redtech/src/pages/ContentByTopic.jsx
+++ redtech/src/pages/ContentByTopic.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router';
 import { useState, useEffect } from 'react';
 import { Pixabay } from "../services/pixabay";
 import HeaderWithCompanyLogo from "../components/HeaderWithCompanyLogo";
